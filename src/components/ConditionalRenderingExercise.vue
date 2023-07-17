dd80ebad129da071c0eb206a0a0d1540d2425ea2
Jasper Delight <jasperdelightt@gmail.com>
2023-07-17 14:48:25 -0600

    basics

--- src/components/ConditionalRenderingExercise.vue
+++ src/components/ConditionalRenderingExercise.vue
@@ -10,7 +10,7 @@
             screen.
           </p>
           <!-- add code to element below -->
-          <div class="player-card text-center">
+          <div v-if="true" class="player-card text-center">
             <div>
               <img :src="state.player.photo" />
             </div>
@@ -38,7 +38,7 @@
             toggle the value to true to get it to show up again.
           </p>
           <!-- add code to element below -->
-          <div class="player-card text-center">
+          <div class="player-card text-center" v-if="state.player.showPlayer">
             <div>
               <img :src="state.player.photo" />
             </div>
@@ -68,23 +68,16 @@
           </p>
           <!-- v-if comparing grade and 90 -->
           <div>
-            <p>The grade is 'A'.</p>
-          </div>
+            <p v-if="state.grade>=90">The grade is 'A'.</p>
+          
           <!-- v-else-if comparing grade and 80 -->
-          <div>
-            <p>The grade is 'B'.</p>
-          </div>
+            <p v-else-if="state.grade >= 80">The grade is 'B'.</p>
           <!-- v-else-if comparing grade and 70 -->
-          <div>
-            <p>The grade is 'C'.</p>
-          </div>
+            <p v-else-if="state.grade >=70">The grade is 'C'.</p>
           <!-- v-else-if comparing grade and 60 -->
-          <div>
-            <p>The grade is 'D'.</p>
-          </div>
+            <p v-else-if="state.grade >=60" >The grade is 'D'.</p>
           <!-- v-else to display if all the others fail -->
-          <div>
-            <p>The grade is 'F'.</p>
+            <p v-else>The grade is 'F'.</p>
           </div>
         </div>
         <div class="border p-1">
@@ -93,7 +86,7 @@
             Change the v-if directive to a v-show on the "player-card" element
             below.
           </p>
-          <div class="player-card text-center area" v-show="state.player.id">
+          <div class="player-card text-center area" v-show="true">
             <div>
               <img :src="state.player.photo" />
             </div>
@@ -132,11 +125,13 @@ export default {
       //add a property to toggle the player-card here.
       //add a property to set the number value here.
       player: {
+        showPlayer: true,
         photo: "https://robohash.org/Mick",
         name: "Mick",
         position: "WR",
         number: 4,
       },
+      grade: 50
     });
     return {
       state,
